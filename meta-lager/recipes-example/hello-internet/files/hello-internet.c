4a920b43c9a1152b86975846b7b755c3104a9a7e
Robert Krantz <krantzr@hotmail.se>
2024-10-25 08:29:39 +0000

    modified:   meta-lager/conf/distro/custom-distro.conf 	modified:   meta-lager/recipes-example/hello-internet/files/hello-internet.c

--- meta-lager/recipes-example/hello-internet/files/hello-internet.c
+++ meta-lager/recipes-example/hello-internet/files/hello-internet.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 int main() {
+    // Send Hello internet with wget to webhook
     system("wget --post-data=\"message=Hello, Internet!\" https://webhook.site/677a88f4-25a9-4b28-8287-85312e730460 -O /dev/null");
     return 0;
 }
